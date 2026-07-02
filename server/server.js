94f60f2d70556de1f69a2018354b9295270d9e0a
NASEYIO <agathanaseyio@gmail.com>
2026-07-02 14:14:47 +0300

    Changes to backend server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // FILE: server.js
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
